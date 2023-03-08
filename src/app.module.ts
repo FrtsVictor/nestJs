52eb38124d8fb993bf256317a32e913dfe081318
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-03-08 16:36:16 -0300

    Added configuration module and nestResponseTransformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './commons/filter/exception/http-exception.filter';
+import { ConfigurationModule } from './config/configuration.module';
+import { TransformNestResponseInterceptor } from './core/http/transform-nest-response.interceptor';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigurationModule, UserModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
     {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
+      provide: APP_INTERCEPTOR,
+      useClass: TransformNestResponseInterceptor,
     },
   ],
 })
