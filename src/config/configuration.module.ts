bcc9c37b12e3436889f95b2e1c44b9af6910c900
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-03-16 00:31:01 -0300

    Added node env to run .envs

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -4,9 +4,12 @@ import databaseConfig from './database.config';
 import httpConfig from './http.config';
 import { EnvironmentService } from './environment.service';
 
+const environmentToLoad = `${process.cwd()}/.env.${process.env.NODE_ENV}`;
+console.log(environmentToLoad);
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: environmentToLoad,
       load: [databaseConfig, httpConfig],
     }),
   ],
