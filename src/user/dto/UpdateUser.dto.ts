9dd3fc3ac202f0d7f7d42090fdb37901f5e56c94
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-03-22 08:13:46 -0300

    Updated props to optional

--- src/user/dto/UpdateUser.dto.ts
+++ src/user/dto/UpdateUser.dto.ts
@@ -4,14 +4,14 @@ import { UniqueEmail } from '../validator/UniqueEmail.validator';
 export class UpdateUserDto {
   @IsOptional()
   @IsNotEmpty({ message: 'Invalid name' })
-  name: string;
+  name?: string;
 
   @IsEmail()
   @IsOptional()
   @UniqueEmail({ message: 'email not unique' })
-  email: string;
+  email?: string;
 
   @MinLength(4)
   @IsOptional()
-  password: string;
+  password?: string;
 }
