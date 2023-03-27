0bed666ab5dcb2892698a923fe38fa9bca507949
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-03-27 11:59:18 -0300

    Imported new modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,15 +1,24 @@
-import { AuthModule } from './auth/auth-module';
+import { AuthModule } from './auth/auth.module';
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { HttpExceptionFilter } from '../commons/filter/exception/http-exception.filter';
+import { HttpExceptionFilter } from '../core/filter/exception/http-exception.filter';
 import { ConfigurationModule } from '../core/config/configuration.module';
 import { TransformNestResponseInterceptor } from '../core/http/transform-nest-response.interceptor';
 import { UserModule } from './users/user.module';
 import { JwtAuthGuard } from '../core/guards/jwt-auth.guard';
 import { PrismaModule } from '../database/prisma.module';
+import { RoleModule } from './roles/role.module';
+import { UserRolesModule } from './user-roles/user-roles.module';
 
 @Module({
-  imports: [ConfigurationModule, PrismaModule, AuthModule, UserModule],
+  imports: [
+    ConfigurationModule,
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    RoleModule,
+    UserRolesModule,
+  ],
   controllers: [],
   providers: [
     {
