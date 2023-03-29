05682a07644e2dbf1f974d9329dd634f50bf83bc
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-03-29 10:38:45 -0300

    Added JwtStrategy tests

---
+++ test/modules/auth/jwt.strategy.spec.ts
@@ -0,0 +1,40 @@
+import { AuthenticatedUser } from '@app-modules/auth/dto/authenticated-user';
+import { JwtStrategy } from '@app-modules/auth/jwt.strategy';
+import { ForbiddenException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AuthUtils } from './auth-utils.mock';
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [JwtStrategy],
+    }).compile();
+
+    jwtStrategy = moduleRef.get<JwtStrategy>(JwtStrategy);
+  });
+
+  it('should be defined', async () => {
+    expect(jwtStrategy).toBeDefined();
+  });
+
+  describe('JwtStrategy.Validate', () => {
+    it('When correctly payload should return authenticatedUser', async () => {
+      const validUser = AuthUtils.giveMe().authenticatedUserMock;
+      const expectedResult = await jwtStrategy.validate(validUser);
+
+      expect(expectedResult.email).toEqual(validUser.email);
+      expect(expectedResult.sub).toEqual(validUser.sub);
+      expect(expectedResult.iat).not.toBeDefined();
+    });
+
+    it('When incorrectly payload should throw ForbiddenException', async () => {
+      const validUser: AuthenticatedUser = { email: null, sub: null };
+
+      await expect(jwtStrategy.validate(validUser)).rejects.toThrow(
+        new ForbiddenException('Invalid payload'),
+      );
+    });
+  });
+});
