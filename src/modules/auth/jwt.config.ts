73c34948da331182a2ff41404dfaee5daef901c4
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-04-03 16:17:28 -0300

    Updated bearer to use 60m

--- src/modules/auth/jwt.config.ts
+++ src/modules/auth/jwt.config.ts
@@ -2,6 +2,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 
 export const jtwConfigModule = JwtModule.register({
-  signOptions: { expiresIn: '60s' },
+  signOptions: { expiresIn: '60m' },
   secretOrPrivateKey: jwtConstants.secret,
 });
