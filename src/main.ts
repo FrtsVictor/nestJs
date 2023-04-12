ed9f415e1155cfd2295e1483854f6245d7d95806
atos_victor <extern.victor.freitas@volkswagen.com.br>
2023-04-12 15:49:49 -0300

    added log verbose and app port by environment

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from '@app-modules/app.module';
 import { ApplicationLogger } from 'src/commons/api/logger/application-logger.log';
+import { AppEnvironmentService } from 'src/commons/api/config/environment.service';
 
 require('module-alias/register');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    logger: ['verbose'],
   });
 
+  const {
+    httpConfig: { port },
+  } = app.get(AppEnvironmentService);
+
   configureApplication(app);
-  const port = 3001;
-  console.log(`Listening on port: ${port}`);
   await app.listen(port);
 }
 
@@ -21,8 +25,8 @@ function configureApplication(app: INestApplication) {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      whitelist: true, //qd for usar o pipe vai ignorar td q vier a mais no request
-      forbidNonWhitelisted: false, //pra dar erro se n iver a prop q a gente mapeia pra receber
+      whitelist: true,
+      forbidNonWhitelisted: false,
     }),
   );
 
